7ff847942fc80b9ec102f5cd5f962c929d309366
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2026-01-08 14:42:35 +0900

    feat: 비밀번호 암호화 기능 추가

---
+++ src/main/java/com/example/concertreservation/user/domain/Password.java
@@ -0,0 +1,49 @@
+package com.example.concertreservation.user.domain;
+
+import com.example.concertreservation.global.error.errorcode.UserErrorCode;
+import com.example.concertreservation.global.error.exception.GlobalException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class Password {
+
+  public static final String HASHED_ALGORITHM = "sha-256";
+
+  @Column(name = "password", nullable = false)
+  private String hashedPassword;
+
+  public Password(String hashedPassword) {
+    this.hashedPassword = hashedPassword;
+  }
+
+  public static Password hashPassword(String password) {
+    return new Password(hash(password));
+  }
+
+  private static String hash(String password) {
+    try {
+      MessageDigest md = MessageDigest.getInstance(HASHED_ALGORITHM);
+      // 바이트 배열에 저장
+      byte[] bytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+      StringBuilder hexStr = new StringBuilder();
+      // byte to 16
+      for (byte b : bytes) {
+        hexStr.append(String.format("%02x", b));
+      }
+      return hexStr.toString();
+
+    } catch (NoSuchAlgorithmException e) {
+      throw new GlobalException(UserErrorCode.INVALID_PASSWORD_ALGORITHM);
+    }
+  }
+}
--- src/main/java/com/example/concertreservation/user/domain/User.java
+++ src/main/java/com/example/concertreservation/user/domain/User.java
@@ -1,7 +1,8 @@
 package com.example.concertreservation.user.domain;
 
-import com.example.concertreservation.global.entity.BaseEntity;
+import com.example.concertreservation.global.entity.SoftDeletedDomain;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,35 +15,36 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "users")
 @Getter
-@NoArgsConstructor(access= AccessLevel.PROTECTED)
-public class User extends BaseEntity {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends SoftDeletedDomain {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(name= "email")
+  @Column(name = "email", unique = true)
   private String email;
 
-  @Column(name = "password")
-  private String password;
+  @Embedded
+  private Password password;
 
-  @Column(name= "name")
+  @Column(name = "name")
   private String name;
 
-  @Column(name= "nickname")
+  @Column(name = "nickname", unique = true)
   private String nickName;
 
-  @Column(name= "point")
+  @Column(name = "point")
   private Long point;
 
   public User(String email, String password, String name, String nickName) {
     this.email = email;
-    this.password = password;
+    this.password = Password.hashPassword(password);
     this.name = name;
     this.nickName = nickName;
-    this.point= 0L;
+    this.point = 0L;
   }
+
 
 }
 
