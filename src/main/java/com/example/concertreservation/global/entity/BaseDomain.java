0c9be05c9f36b0678c1dd5675bb9aa935cb1fc1a
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2026-02-08 16:34:19 +0900

    rename: 파일명 변경

--- src/main/java/com/example/concertreservation/global/entity/BaseDomain.java
+++ src/main/java/com/example/concertreservation/global/entity/BaseDomain.java
@@ -11,11 +11,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @Getter
 @EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
+public class BaseDomain {
 
-  @CreatedDate
-  private LocalDateTime createdAt;
+    @CreatedDate
+    private LocalDateTime createdAt;
 
-  @LastModifiedDate
-  private LocalDateTime updatedAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
