4d76cabef4b896e094fac7fdf687e23f1c965c23
SeoyeonLee <leeseoyeon10116@gmail.com>
2026-02-13 11:07:44 +0900

    refector: 폴더 구조 변경

--- src/main/java/com/example/concertreservation/auth/config/AuthConfig.java
+++ src/main/java/com/example/concertreservation/auth/config/AuthConfig.java
@@ -1,10 +1,12 @@
-package com.example.concertreservation.auth;
+package com.example.concertreservation.auth.config;
 
-import java.util.List;
+import com.example.concertreservation.auth.AuthArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
