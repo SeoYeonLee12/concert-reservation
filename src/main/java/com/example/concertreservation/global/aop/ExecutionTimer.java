700ab065a3e5be1f05ace693ed4745408af5994c
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2026-02-20 18:39:25 +0900

    feat: 메소드 실행시간을 측정할 AOP 정의

---
+++ src/main/java/com/example/concertreservation/global/aop/ExecutionTimer.java
@@ -0,0 +1,37 @@
+package com.example.concertreservation.global.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Slf4j
+@Aspect
+@Component
+public class ExecutionTimer {
+
+    @Around("timer()")
+    public Object AssumeExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+        Object result = joinPoint.proceed();
+        stopWatch.stop();
+
+        long totalTimeMillis = stopWatch.getTotalTimeMillis();
+
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        String methodName = signature.getMethod().getName();
+
+        log.info("Execution Method: {}, Execution Time: {}", methodName, totalTimeMillis);
+        return result;
+    }
+
+    @Pointcut("@annotation(com.example.concertreservation.global.aop.ExecutionTime)")
+    private void timer() {
+    }
+}
